0ff4220412da6d443d48b9cd0cd8151a06e2faaa
LenartL <99958904+Llavrih@users.noreply.github.com>
2023-11-13 10:38:25 +0100

    Added filename arg

--- server.py
+++ server.py
@@ -4,7 +4,12 @@ import socket
 def server():
     host = "192.168.75.165"  # Server's IP address
     port = 12346
-    filename = "carina_nebula.zip"  # Name of the file to send
+    default_filename = "carina_nebula.zip"  # Default file name
+
+    # Prompt for a filename, use default if no input is given
+    filename = input(f"Enter filename (or press enter to use '{default_filename}'): ")
+    if not filename:
+        filename = default_filename
 
     with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
         s.bind((host, port))
@@ -14,13 +19,15 @@ def server():
             print(f"Connected by {addr}")
 
             # Open and read the file
-            with open(filename, "rb") as file:
-                file_data = file.read()
+            try:
+                with open(filename, "rb") as file:
+                    file_data = file.read()
 
-            # Send the file data
-            conn.sendall(file_data)
-
-            print(f"File {filename} sent successfully.")
+                # Send the file data
+                conn.sendall(file_data)
+                print(f"File {filename} sent successfully.")
+            except FileNotFoundError:
+                print(f"Error: File '{filename}' not found.")
 
 
 if __name__ == "__main__":
